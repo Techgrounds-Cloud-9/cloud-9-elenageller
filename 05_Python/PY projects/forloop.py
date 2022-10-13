e8d5efb7d2818d1836935363a02f47194a406ae8
elenageller <114391815+elenageller@users.noreply.github.com>
2022-10-13 14:57:57 +0200

    py04

---
+++ 05_Python/PY projects/forloop.py
@@ -0,0 +1,10 @@
+
+x = 5 
+for i in range(50):
+    print(x*i)
+
+
+
+    #Print the value of i in the for loop. 
+    # You did not manually assign a value to i. 
+    # Figure out how its value is determined.
