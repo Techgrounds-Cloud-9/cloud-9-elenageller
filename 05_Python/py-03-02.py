17ee7596d44448eef34a4a169bd18fce6fea1226
elenageller <114391815+elenageller@users.noreply.github.com>
2022-10-13 13:48:34 +0200

    py-03

---
+++ 05_Python/py-03-02.py
@@ -0,0 +1,11 @@
+#take a value from a user
+name = input("What is your name? ")
+age = input("How old are you? ")
+#display the value on a screen (its always a str)
+print(type(name))
+print(type(age))
+
+#we can convert input type in int
+age2 = int(input("How old are you? "))
+
+print(type(age2))
---
+++ 05_Python/py-03.py
@@ -0,0 +1,23 @@
+# A string value assigned to a varible a
+a = 'int'
+
+# An int value assigned to a varible b
+b = 7
+
+# A boolean value assigned to a varible c
+c = False
+
+# A string valuable assigned to a varible d
+d = "18.5"
+
+#print types of those variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#converting other types to float
+x = float(b) + float(d) 
+
+#print the value of variable x
+print (x)
