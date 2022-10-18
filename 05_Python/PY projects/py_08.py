aae5dc77efdfea4e5fbd5ab4d518356e883053d2
elenageller <114391815+elenageller@users.noreply.github.com>
2022-10-18 15:41:16 +0200

    update 08

---
+++ 05_Python/PY projects/py_08.py
@@ -0,0 +1,11 @@
+test_dict = {
+  "First name": "Casper",
+  "Last name": "Velzen",
+  "Job title": "Lead Learning Coach",
+  "Company": "TechGrounds"
+}
+
+for k, v in test_dict.items():
+    print(k,v)
+
+
